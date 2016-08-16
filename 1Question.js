72ba5ce0efa32febb387d1aeb47e906177230adb
Abdulrasaq Nasirudeen <nosisky@gmail.com>
2016-08-16 16:24:50 +0100

    Create 1Question.js

---
+++ 1Question.js
@@ -0,0 +1,1 @@
+You are to write a function, reverseString(string), that returns the reverse of the string provided. If the reverse of the string is the same as the original string, as in the case of palindromes, return true instead.
