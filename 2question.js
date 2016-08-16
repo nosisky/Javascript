dfa8f2f4cfa70776e470ea4180e0751549ec844e
Abdulrasaq Nasirudeen <nosisky@gmail.com>
2016-08-16 17:19:20 +0100

    Create 2question.js

---
+++ 2question.js
@@ -0,0 +1,12 @@
+## Challenge
+Have the function LetterChanges(str) take the str parameter being passed and modify it
+using the following algorithm. Replace every letter in the string with the letter
+following it in the alphabet (i.e. c becomes d, z becomes a). Then capitalize every
+vowel in this new string (a, e, i, o, u) and finally return this modified string.
+
+### Test case
+> INPUT: 'zebra'
+  OUTPUT: 'Afcsb'
+
+> INPUT: 'houSe'
+OUTPUT: 'Ipvtf'
